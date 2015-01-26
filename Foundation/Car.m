1804d96c43b88b548e973ec9cb107169201ea35c
Fadie Hannona <fadiehannona@gmail.com>
2015-01-26 22:48:59 +0000

    instantiation

--- Foundation/Car.m
+++ Foundation/Car.m
@@ -6,16 +6,22 @@
 //  Copyright (c) 2015 Fadie. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #import "Car.h"
 
-@implementation Car {
-    double _odometer;
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        Car *toyota = [[Car alloc] init];
+        
+        [toyota setModel:@"Toyota Corolla"];
+        NSLog(@"Created a %@", [toyota model]);
+        
+        toyota.model = @"Toyota Camry";
+        NSLog(@"Changed the car to a %@", toyota.model);
+        
+        [toyota drive];
+        
+    }
+    
+    return 0;
 }
-
-@synthesize model = _model;
-
-- (void)drive {
-    NSLog(@"Driving a %@. Vroooom!", self.model);
-}
-
-@end
--- Foundation/functions.m
+++
@@ -1,9 +0,0 @@
-//
-//  functions.m
-//  Foundation
-//
-//  Created by Fadie on 22/01/2015.
-//  Copyright (c) 2015 Fadie. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
